f78b08594d70319d6fab5d1ea34ebac37f607d6c
joao <121593043+depvasco@users.noreply.github.com>
2023-07-25 00:23:46 -0300

    Update main.py
    debug session

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from discord import app_commands
 from moviepy.editor import *
 
 #INSERT THE BOT TOKEN HERE
-bot_tk = ''
-
+bot_tk = 'MTA2NDc1MDQzNzkzMDExMTAxNw.GT9g6w.oEr8O0zqFepUedEuDf1gBw3Jn_sKwYkIV5IKsU'
+guild_id = 1064743637205921832
 #STARTING THE BOT
 class MyClient(discord.Client):
     def __init__(self):
@@ -16,7 +16,7 @@ class MyClient(discord.Client):
     async def on_ready(self):
         await self.wait_until_ready()
         if not self.synced:
-            await tree.sync(guild = discord.Object(id = 1055071610463334470))
+            await tree.sync(guild = discord.Object(id = guild_id))
             self.synced = True
         print(f'Discord bot logged on {self.user} as sucesseful!')
 
@@ -25,20 +25,24 @@ tree = app_commands.CommandTree(client)
 
 #CUTVIDEO SLASH COMMAND SCRUTURE
 #ps: you need to change the guild id so then you can see at commands list
-@tree.command(name='cutvideo', description='Cuts a determinated space of a video.', guild = discord.Object(id= INSERT YOUR GUILD ID HERE))
-async def self(interaction: discord.Interaction, video_url: str, start: int = None, ends: int = None):
+@tree.command(name='cutvideo', description='Cuts a determinated space of a video.', guild = discord.Object(id= guild_id))
+async def self(interaction: discord.Interaction, video_url: str, start: int = 0, ends: int = None):
 #SIMPLE VERIFICATION IF IT ENDS WITH .mp4 or .wav EXT.
     if video_url.endswith('.mp4' or '.wav'):
         await interaction.response.defer()
 #MAKING THE VIDEO (USES YOUR CPU)
-        videobefore = VideoFileClip(video_url)
-        newvideo = videobefore.subclip(t_start= start, t_end= ends)
-        newvideo.write_videofile(filename='produced.mp4')
-#IT WILL GO TO THE SOURCE FOLDER, AND WILL BE UPLOADED TO DISCORD
-        await interaction.followup.send(file=discord.File('produced.mp4'))   
+        try:
+            videobefore = VideoFileClip(video_url)
+            newvideo = videobefore.subclip(t_start= start, t_end= ends)
+            newvideo.write_videofile(filename='produced.mp4')
+
+        #IT WILL GO TO THE SOURCE FOLDER, AND WILL BE UPLOADED TO DISCORD   
+        except:
+                await interaction.response.send_message('Something get wrong.')
     else:
-        await interaction.response.send_message('We cant indentify this file as a MP4 or WAV file.')
+         await interaction.response.send_message('We cant indentify this file as a MP4 or WAV file.')
+
+    await interaction.followup.send(file=discord.File('produced.mp4'))
 
 #RUNS THE BOT.
 client.run(bot_tk)
-
